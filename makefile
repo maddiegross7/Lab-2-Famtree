9153dd0b64b321dfcc83bc7f43baebdfc0a03fc5
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-02-04 14:05:38 -0500

    okay we are going places

--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 CC = gcc 
 INCLUDES = -I/home/mrjantz/cs360/include
+# INCLUDES = -I/home/mgross10/cs360/cs360-lecture-notes/Libfdr/include
 CFLAGS = $(INCLUDES)
 LIBDIR = /home/mrjantz/cs360/lib
 LIBS = $(LIBDIR)/libfdr.a 
--- src/famtree.c
+++ src/famtree.c
@@ -6,6 +6,8 @@
 #include "dllist.h"
 #include "jrb.h"
 
+//./bin/famtree < data/fam1.txt
+
 typedef struct person{
     char *name;
     char sex;
@@ -15,10 +17,6 @@ typedef struct person{
 
     int visited;
 } Person;
-
-int doesPersonExist(){
-
-}
 
 int main(int argc, char const *argv[])
 {
@@ -38,17 +36,45 @@ int main(int argc, char const *argv[])
 
     while(get_line(is) >= 0){
         //printf("Number of Fields: %i", is->NF);
-        // printf("%d: %s", is->line, is->text1);
+        //printf("%d: %s", is->line, is->text1);
         // if (is->text1[0] != '\n'|| is->text1[0] != '\0') {
-        //     printf("Field 1: %s, Field 2: %s\n\n", is->fields[0], is->fields[1]);
+        printf("Field 1: %s, Field 2: %s\n\n", is->fields[0], is->fields[1]);
         // }
+        
+        if(strcmp(is->fields[0],"SEX")!=0){
+            // if (temp == NULL) {
+            //     printf("temp is NULL\n");
+            // } else {
+            //     printf("Before seek: %s\n", temp->val);
+            // }
+            // //printf("Before seek: %s", temp->val);
+            temp = jrb_find_str(people, is->fields[1]);
+            // //printf("After seek: %s", temp->val);
+            // if (temp == NULL) {
+            //     printf("temp is NULL\n");
+            // } else {
+            //     printf("After seek: %s\n", temp->val);
+            // }
 
-        if(strcmp(is->fields[0],"SEX")){
-            if(jrb_find_str(people, is->fields[1]) == NULL){
+            if(temp == NULL){
                 printf("%s has not been created yet\n", is->fields[1]);
+
+                Person *newPerson = (Person *)malloc(sizeof(Person));
+
+                newPerson->name = strdup(is->fields[1]);
+                
+                //printf("Field 1 hellllloooooooo: %s\n", is->fields[1]);
+                jrb_insert_str(people, newPerson->name, new_jval_v((void *) newPerson));
+
+                // if(strcmp(is->fields[0],"PERSON")==0)
+                //currentPerson = newPerson;
             }
         }
+    }
 
+    jrb_traverse(temp, people) {
+        currentPerson = (Person *) temp->val.v;
+        printf("%-40s\n", currentPerson->name);
     }
 
     return 0;
