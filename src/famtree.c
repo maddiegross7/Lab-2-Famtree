6f8a49693e41879373cf2cac6469271383ea955d
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-02-04 12:30:29 -0500

    we are now understanding how we should do at least the first part

--- src/famtree.c
+++ src/famtree.c
@@ -16,21 +16,39 @@ typedef struct person{
     int visited;
 } Person;
 
+int doesPersonExist(){
+
+}
+
 int main(int argc, char const *argv[])
 {
     IS is;
-    
+    JRB people, temp;
+
     is = new_inputstruct(NULL);
+    people = make_jrb();
+
     if(is == NULL) {
         perror("file cannot be opened");
     }
 
     printf("hello is is not null\n");
-    
+
+    Person *currentPerson;
+
     while(get_line(is) >= 0){
-        for(int i=0; i < is->NF; i++){
-            printf("%d: %s\n", is->line, is->fields[i]);
+        //printf("Number of Fields: %i", is->NF);
+        // printf("%d: %s", is->line, is->text1);
+        // if (is->text1[0] != '\n'|| is->text1[0] != '\0') {
+        //     printf("Field 1: %s, Field 2: %s\n\n", is->fields[0], is->fields[1]);
+        // }
+
+        if(strcmp(is->fields[0],"SEX")){
+            if(jrb_find_str(people, is->fields[1]) == NULL){
+                printf("%s has not been created yet\n", is->fields[1]);
+            }
         }
+
     }
 
     return 0;
