864340d1a64221510b9e9286f59df70b03be0c2d
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-02-05 22:43:05 -0500

    okay we have passed all the greadscripts and now we just need to free all the memory

--- src/famtree.c
+++ src/famtree.c
@@ -35,7 +35,7 @@ char *createName(IS is){
         return NULL;
     }
 
-    name['\0'];
+    name[0]='\0';
 
     for(int i = 1; i < is->NF; i++){
         strcat(name, is->fields[i]);
@@ -49,7 +49,7 @@ char *createName(IS is){
 
 int findCycle(Person *person){
     if(person->visited == 1){
-        printf("Cycle Detected at %s\n", person->name);
+        fprintf(stderr,"Bad input -- cycle in specification\n");
         return 1;
     }
 
@@ -78,7 +78,7 @@ int main(int argc, char const *argv[])
     people = make_jrb();
 
     if(is == NULL) {
-        perror("file cannot be opened");
+        fprintf(stderr, "file cannot be opened");
         return 1;
     }
 
@@ -96,7 +96,7 @@ int main(int argc, char const *argv[])
             if(temp == NULL){
                 linePerson = (Person *)malloc(sizeof(Person));
                 linePerson->name = name;
-                linePerson->sex = NULL;
+                linePerson->sex = "";
                 linePerson->mother = NULL;
                 linePerson->father = NULL;
                 linePerson->visited = 0;
@@ -108,7 +108,7 @@ int main(int argc, char const *argv[])
             }
             linePerson = (Person *) jrb_find_str(people, name)->val.v;
         }
-        if(linePerson == NULL) perror("idk why but maybe this is the problem");
+        if(linePerson == NULL) fprintf(stderr, "idk why but maybe this is the problem");
         if(strcmp(is->fields[0], "PERSON") == 0){
             currentPerson = linePerson;
             //printf("hello %s is the current player\n\n", currentPerson->name);
@@ -120,13 +120,13 @@ int main(int argc, char const *argv[])
                 dll_append(linePerson->children,new_jval_v((void *) currentPerson));
                 currentPerson->unprintedParents++;
             } else if(strcmp(currentPerson->father->name, linePerson->name) != 0){
-                printf("Bad input -- child with two fathers on line %i\n", is->line);
+                fprintf(stderr, "Bad input -- child with two fathers on line %i\n", is->line);
                 return 1;
             }
-            if(linePerson->sex == NULL){
+            if(linePerson->sex == ""){
                 linePerson->sex = strdup("M");
             } else if(strcmp(linePerson->sex, "M") != 0){
-                printf("Bad input - sex mismatch on line %i\n", is->line);
+                fprintf(stderr, "Bad input - sex mismatch on line %i\n", is->line);
                 return 1;
             }
             
@@ -137,13 +137,13 @@ int main(int argc, char const *argv[])
                 dll_append(linePerson->children,new_jval_v((void *) currentPerson));
                 currentPerson->unprintedParents++;
             } else if(strcmp(currentPerson->mother->name, linePerson->name) != 0){
-                printf("Bad input -- child with two mothers on line %i\n", is->line);
+                fprintf(stderr, "Bad input -- child with two mothers on line %i\n", is->line);
                 return 1;
             }
-            if(linePerson->sex == NULL){
+            if(linePerson->sex == ""){
                 linePerson->sex = strdup("F");
             } else if(strcmp(linePerson->sex, "F") != 0){
-                printf("Bad input - sex mismatch on line %i\n", is->line);
+                fprintf(stderr, "Bad input - sex mismatch on line %i\n", is->line);
                 return 1;
             }
             
@@ -154,36 +154,37 @@ int main(int argc, char const *argv[])
                 dll_append(currentPerson->children,new_jval_v((void *) linePerson));
                 linePerson->unprintedParents++;
             } else if(strcmp(linePerson->mother->name, currentPerson->name) != 0){
-                printf("Bad input -- child with two mothers on line %i\n", is->line);
+                fprintf(stderr, "Bad input -- child with two mothers on line %i\n", is->line);
                 return 1;
             }
-            if(currentPerson->sex == NULL){
+            if(currentPerson->sex == ""){
                 currentPerson->sex = strdup("F");
             } else if(strcmp(currentPerson->sex, "F") != 0){
-                printf("Bad input - sex mismatch on line %i\n", is->line);
+                fprintf(stderr, "Bad input - sex mismatch on line %i\n", is->line);
                 return 1;
             }
         } else if(strcmp(is->fields[0], "FATHER_OF") == 0){
             if(linePerson->father == NULL){
                 linePerson->father = currentPerson;
                 currentPerson->childCount++;
+                //printf("setting %s's father to %s\n", linePerson->name, currentPerson->name);
                 dll_append(currentPerson->children,new_jval_v((void *) linePerson));
                 linePerson->unprintedParents++;
             } else if(strcmp(linePerson->father->name, currentPerson->name) != 0){
-               printf("Bad input -- child with two fathers on line %i\n", is->line);
+               fprintf(stderr, "Bad input -- child with two fathers on line %i\n", is->line);
                return 1;
             }
-            if(currentPerson->sex == NULL){
+            if(currentPerson->sex == ""){
                 currentPerson->sex = strdup("M");
             } else if(strcmp(currentPerson->sex, "M") != 0){
-                printf("Bad input - sex mismatch on line %i\n", is->line);
+                fprintf(stderr, "Bad input - sex mismatch on line %i\n", is->line);
                 return 1;
             }
         } else if(strcmp(is->fields[0], "SEX") == 0){
-            if(currentPerson->sex == NULL){
+            if(currentPerson->sex == ""){
                 currentPerson->sex = strdup(is->fields[1]);
             } else if(strcmp(currentPerson->sex, is->fields[1]) != 0){
-                printf("Bad input - sex mismatch on line %i\n", is->line);
+                fprintf(stderr, "Bad input - sex mismatch on line %i\n", is->line);
                 return 1;
             }
         }
@@ -202,7 +203,7 @@ int main(int argc, char const *argv[])
         }
 
         if(currentPerson->unprintedParents == 0){
-            printf("adding %s to people to print", currentPerson->name);
+            //printf("adding %s to people to print", currentPerson->name);
             dll_append(peopleToPrint, new_jval_v((void *) currentPerson));
         }
     }
@@ -221,41 +222,75 @@ int main(int argc, char const *argv[])
             printf("\tSex: Unknown\n");
         }
 
-    }
-
-
-
-    jrb_traverse(temp, people) {
-        currentPerson = (Person *) temp->val.v;
-        printf("Name: %-40s\n", currentPerson->name);
-        
-        // Print Sex (handle NULL)
-        printf("Sex: %s\n", currentPerson->sex ? currentPerson->sex : "Unknown");
-
-        // Print Father (handle NULL)
-        printf("Father: %s\n", currentPerson->father ? currentPerson->father->name : "None");
-
-        // Print Mother (handle NULL)
-        printf("Mother: %s\n", currentPerson->mother ? currentPerson->mother->name : "None");
-
-        // Print Children (traverse Dllist)
-        if (!dll_empty(currentPerson->children)) {
-            printf("Children: ");
-            Dllist node;
-            dll_traverse(node, currentPerson->children) {
-                Person *child = (Person *) node->val.v;
-                printf("%s%s", child->name, (node->flink != currentPerson->children) ? ", " : ""); 
-            }
-            printf("\n");
-        } else {
-            printf("Children: None\n");
+        if(person->father){
+            printf("\tFather: %s\n", person->father->name);
+        }else{
+            printf("\tFather: Unknown\n");
         }
 
-        // Print visited status
-        printf("Visited: %d\n", currentPerson->visited);
-        
-        printf("--------------------------\n"); // Separator for readability
+        if(person->mother){
+            printf("\tMother: %s\n", person->mother->name);
+        }else{
+            printf("\tMother: Unknown\n");
+        }
+
+        if (!dll_empty(person->children)) {
+            printf("\tChildren: \n");
+            Dllist node;
+            dll_traverse(node, person->children) {
+                //printf("hello");
+                Person *child = (Person *) node->val.v;
+                if(child->name){
+                    printf("\t  %s\n", child->name);
+                }else{
+                    printf("houston we have a problem");
+                }
+                 
+                child->unprintedParents--;
+                if(child->unprintedParents == 0){
+                    dll_append(peopleToPrint, new_jval_v((void *) child));
+                }
+            }
+        } else {
+            printf("\tChildren: None\n");
+        }
+
+        printf("\n");
     }
+
+
+
+    // jrb_traverse(temp, people) {
+    //     currentPerson = (Person *) temp->val.v;
+    //     printf("Name: %-40s\n", currentPerson->name);
+        
+    //     // Print Sex (handle NULL)
+    //     printf("Sex: %s\n", currentPerson->sex ? currentPerson->sex : "Unknown");
+
+    //     // Print Father (handle NULL)
+    //     printf("Father: %s\n", currentPerson->father ? currentPerson->father->name : "None");
+
+    //     // Print Mother (handle NULL)
+    //     printf("Mother: %s\n", currentPerson->mother ? currentPerson->mother->name : "None");
+
+    //     // Print Children (traverse Dllist)
+    //     if (!dll_empty(currentPerson->children)) {
+    //         printf("Children: ");
+    //         Dllist node;
+    //         dll_traverse(node, currentPerson->children) {
+    //             Person *child = (Person *) node->val.v;
+    //             printf("%s%s", child->name, (node->flink != currentPerson->children) ? ", " : ""); 
+    //         }
+    //         printf("\n");
+    //     } else {
+    //         printf("Children: None\n");
+    //     }
+
+    //     // Print visited status
+    //     printf("Visited: %d\n", currentPerson->visited);
+        
+    //     printf("--------------------------\n"); // Separator for readability
+    // }
 
 
     return 0;
